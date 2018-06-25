5a1fb6afe76c80a5798dd4b5b29e695c5498bf01
Loris Gaio <lorisgsantos@yahoo.com.br>
2018-06-25 15:18:32 -0300

    finish config and translate

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -26,7 +26,10 @@ RailsAdmin.config do |config|
   ## To disable Gravatar integration in Navigation Bar set to false
   # config.show_gravatar = true
 
+  config.main_app_name = ["Anjos Protetores", "Canil"]
+
   config.model User do
+    navigation_icon 'fa fa-user'
     create do
       field :name
       field :kind
@@ -34,6 +37,52 @@ RailsAdmin.config do |config|
       field :email
       field :password
       field :password_confirmation
+    end
+
+    edit do
+      field :name
+      field :kind
+      field :status
+      field :email
+      field :password
+      field :password_confirmation
+    end
+
+    list do
+      field :name
+      field :kind
+      field :status
+      field :email
+    end
+  end
+
+  config.model Animal do
+    navigation_icon 'fa fa-paw'
+    list do
+      field :id
+      field :name
+      field :tutor
+      field :breed
+      field :age
+      field :gender
+      # field :castrated
+      # field :vaccinated
+      # field :dewormed
+    end
+  end
+
+  config.model Tutor do
+    navigation_icon 'fa fa-home'
+    list do
+      field :id
+      field :name
+      field :address
+      field :phone
+      field :animals
+      # field :gender
+      # field :castrated
+      # field :vaccinated
+      # field :dewormed
     end
   end
 
